f97761ff164dd03c84da045c9edb844b197a9f4b
Jim Maroulis <jmaroulis@redlattice.com>
2022-12-01 07:48:26 -0500

    fix dvd

--- app/models/dvd.py
+++ app/models/dvd.py
@@ -4,8 +4,9 @@ from app import db
 class DVD(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), index=True)
-    release_date = db.Column(db.String(128), index=True)
+    format = db.Column(db.String(128), index=True)
     director = db.Column(db.String(128), index=True)
+    release = db.Column(db.String(128), index=True)
 
     display_name = 'DVDs'
 
@@ -16,13 +17,16 @@ class DVD(db.Model):
         return {
             'id': self.id,
             'title': self.title,
-            'release_date': self.release_date,
-            'director': self.director
+            'director': self.director,
+            'format': self.format,
+            'release': self.release,
         }
 
     @staticmethod
     def field_names() -> list:
-        return ['title', 'release_date', 'director']
+        # so if this order is changed the display on the page is wrong, format for director and vise versa
+        # why?
+        return ['title', 'director', 'format', 'release']
 
     @staticmethod
     def display_names() -> list:
@@ -31,8 +35,9 @@ class DVD(db.Model):
         return names
 
     def from_dict(self, data: dict) -> bool:
-        self.title = data.get('title') if data.get('title') else self.make
-        self.director = data.get('director') if data.get('director') else self.model
-        self.release_date = data.get('release_date') if data.get('release_date') else self.socket
+        self.title = data.get('title') if data.get('title') else self.title
+        self.director = data.get('director') if data.get('director') else self.director
+        self.format = data.get('format') if data.get('format') else self.format
+        self.release = data.get('release') if data.get('release') else self.release
 
         return True
