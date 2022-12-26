edbb486f478ea17ff9c38adf1d60714220a0c4eb
JIm Maroulis <jjmaroulis@gmail.com>
2022-12-26 06:59:52 -0500

    making a more dynamic extensible thing

---
+++ app/vault_81.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    pass
+    # load the database (jsonl) files
+# load all the files found in database directory
+# import the model for each one based on the file name
+# only files existing will show up in web page
---
+++ scripts/regen_venv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# check if venv active and deactivate
+# check if venv exists at target location
+# delete existing - TODO: make this switchable option later
+# make new venv
+# activate new venv
+# update pip
+# install requirements.txt
